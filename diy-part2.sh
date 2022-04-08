4d88653ad7958fb5c5edd37161e19f8d32054662
ericvwv <73857090+ericvwv@users.noreply.github.com>
2022-04-08 18:08:49 +0800

    Create diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,1 +1,4 @@
-#
+# 修改连接数数
+sed -i 's/net.netfilter.nf_conntrack_max=.*/net.netfilter.nf_conntrack_max=65535/g' package/kernel/linux/files/sysctl-nf-conntrack.conf
+#修正连接数（by ベ七秒鱼ベ）
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package/base-files/files/etc/sysctl.conf
